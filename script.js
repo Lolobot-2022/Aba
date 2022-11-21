6d6b068591f8aee321fd69030839766bd2d8b445
H-movies <angelocambry@gmail.com>
2022-11-21 01:02:53 +0100

    third

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const lightMode = document.getElementById('light-mode');
+// const counterUp = require("counterup");
 
 lightMode.addEventListener('change', () => {
   document.body.classList.toggle('light');
-})
+}) //Pour le light Mode
+
+
+//pour l'animation du nombre de personnes tuees en 2022
+// counterUp.addEventListener('counter', () =>{
+//    document.body.classList.toggle('num');
+// })
