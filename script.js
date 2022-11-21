21c31addf16e6601dd5359e3d39f41b8bc3e1b7a
H-movies <angelocambry@gmail.com>
2022-11-21 18:53:48 +0100

    fith

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const lightMode = document.getElementById('light-mode');
+window.onscroll = function() {myFunctionOne()};
 let counts=setInterval(updated);
 let countsone=setInterval(updatedOne);
 let upto=1000;
 let uptoOne=500;
+var divs = ["graph0","graph1", "graph2", "graph3", "graph4", "graph5"];
+var visibleId = null;
+const ctx = document.getElementById('myChart');
+const ctxx =document.getElementById('myChartOne');
+
+
+
 
 
 lightMode.addEventListener('change', () => {
@@ -27,5 +35,94 @@ function updatedOne (){
     clearInterval(countsone);
   }
 }
+
+
+function myFunctionOne() {
+  if (document.documentElement.scrollTop > 10) {
+    document.getElementById("objectif").style.display = "animation";
+  } 
+}
+
+//POUR AFFICHER OU MASQUER LES GRAPHIQUES
+
+function show(id) {
+  if(visibleId !== id) {
+    visibleId = id;
+  } 
+  hide();
+}
+function hide() {
+  var div, i, id;
+  for(i = 0; i < divs.length; i++) {
+    id = divs[i];
+    div = document.getElementById(id);
+    if(visibleId === id) {
+      div.style.display = "block";
+    } else {
+      div.style.display = "none";
+    }
+  }
+}  
+
+//POUR LES GRAPHIQUES QUI VIENNENT DE CHART.JS
+
+//POUR LES TUES
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Ce Mois','3 Mois', '6 Mois', '1 An','5 Ans'],
+    datasets: [{
+      label: '# de tués',
+      data: [12, 19, 3, 5, 2],
+      borderWidth: 1,
+      backgroundColor:[
+        "crimson",
+        "lightgreen",
+        "lightblue",
+        "violet", 
+        "gray"
+      ]
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+//POUR LES KIDNAPES
+new Chart(ctxx, {
+  type: 'bar',
+  data: {
+    labels: ['Ce Mois','3 Mois', '6 Mois', '1 An','5 Ans'],
+    datasets: [{
+      label: '# de kidnapés',
+      data: [6, 28, 10, 20, 32],
+      borderWidth: 1,
+      backgroundColor:[
+        "green",
+        "lightblue",
+        "lightyellow",
+        "pink", 
+        "red"
+      ]
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+
+
+
+
 
 
