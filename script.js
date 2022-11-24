3c4dc909510255e7648b2da941e56ac45b2fd0b8
H-movies <angelocambry@gmail.com>
2022-11-24 06:33:23 +0100

    nineth

--- script.js
+++ script.js
@@ -2,16 +2,17 @@ const lightMode = document.getElementById('light-mode');
 window.onscroll = function() {myFunctionOne()};
 let counts=setInterval(updated);
 let countsone=setInterval(updatedOne);
-let upto=1000;
-let uptoOne=500;
+let upto=4900;
+let uptoOne=1;
 var divs = ["graph0","graph1", "graph2", "graph3", "graph4", "graph5"];
 var visibleId = null;
 const ctx = document.getElementById('myChart');
+const continent =document.getElementById('forContinent');
 const ctxx =document.getElementById('myChartOne');
 const line =document.getElementById('line');
 const lineone =document.getElementById('lineOne');
-const lineTrois =document.getElementById('lineTrois');
-const lineTroisBis =document.getElementById('lineTroisBis');
+// const lineTrois =document.getElementById('lineTrois');
+// const lineTroisBis =document.getElementById('lineTroisBis');
 
 
 
@@ -27,7 +28,7 @@ lightMode.addEventListener('change', () => {
 function updated (){
   var count =document.getElementById("numb");
   count.innerHTML=++upto;
-  if (upto===1200)
+  if (upto===5060)
   {
     clearInterval(counts);
   }
@@ -36,7 +37,7 @@ function updated (){
 function updatedOne (){
   var count2 =document.getElementById("numb2");
   count2.innerHTML=++uptoOne;
-  if (uptoOne===1000)
+  if (uptoOne===3)
   {
     clearInterval(countsone);
   }
@@ -72,41 +73,106 @@ function hide() {
 
 //POUR LES GRAPHIQUES QUI VIENNENT DE CHART.JS
 
-//BAR POUR LES TUES
+//BAR POUR LES PAYS
 new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ['Ce Mois','3 Mois', '6 Mois', '1 An','5 Ans'],
+    labels: ['France','Canada', 'Turquie', 'Etats-Unis','Bresil','Cuba','Chili','Allemagne','Russie','Italie','Royaume-Uni'],
     datasets: [{
-      label: '# de tués',
-      data: [12, 19, 25, 35, 42],
+      label: '# étudiants haitiens par pays',
+      data: [1685, 453, 51, 1066, 992, 131, 544, 19, 31, 74, 14],
       borderWidth: 1,
       backgroundColor:[
         "crimson",
         "lightgreen",
         "lightblue",
         "violet", 
-        "gray"
+        "gray",
+        "pink",
+        "whitesmoke",
+        "yellow",
+        "purple",
+        "brown",
+        "tan"
       ]
     }]
   },
   options: {
     scales: {
       y: {
-        beginAtZero: true
+        beginAtZero: true,
+        suggestedMax:100,
+        ticks:{
+          font: {
+            size:10
+          }
+        }
+       
+      },
+      x:{
+        ticks:{
+          font:{
+            size:10
+          }
+          
+        }
       }
+
     }
   }
 });
 
-//BAR POUR LES KIDNAPES
+//BAR POUR LES CONTINENTS
+new Chart(continent, {
+  type: 'bar',
+  data: {
+    labels: ['Europe','Amérique', 'Asie', 'Afrique','Océanie'],
+    datasets: [{
+      label: '# étudiants haitiens par continent',
+      data: [1786, 3186, 0, 0, 0],
+      borderWidth: 1,
+      backgroundColor:[
+        "whitesmoke",
+        "yellow",
+        "purple",
+        "brown",
+        "indigo"
+      ]
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+        suggestedMax:100,
+        ticks:{
+          font: {
+            size:10
+          }
+        }
+       
+      },
+      x:{
+        ticks:{
+          font:{
+            size:10
+          }
+          
+        }
+      }
+
+    }
+  }
+});
+
+//BAR POUR LES INSCRITS SUR ELITIS
 new Chart(ctxx, {
   type: 'bar',
   data: {
     labels: ['Ce Mois','3 Mois', '6 Mois', '1 An','5 Ans'],
     datasets: [{
-      label: '# de kidnapés',
-      data: [6, 58, 10, 30, 62],
+      label: '# étudiants inscrits sur Elitis',
+      data: [3, 0, 0, 0, 0],
       borderWidth: 1,
       backgroundColor:[
         "green",
@@ -120,7 +186,20 @@ new Chart(ctxx, {
   options: {
     scales: {
       y: {
-        beginAtZero: true
+        beginAtZero: true,
+        suggestedMax:100,
+        ticks:{
+          font:{
+            size:10
+          }
+        }
+      },
+      x:{
+        ticks:{
+          font:{
+            size:10
+          }
+        }
       }
     }
   }
@@ -204,81 +283,80 @@ new Chart(line,{
 
 //LINE POUR LES TUES 3 MOIS
 
-new Chart(lineTrois,{
-  type:'line',
-  data: {
-    labels:["3 nov 22", "10 nov 22", "19 nov 22","30 nov 22"],
-    datasets:[{
-    label:'# de tués',
-    data:[5, 55, 15, 67],
-    fill:true,
-    backgroundColor:"yellow",
-    tension:0.1
-  }]
-},
-  options:{
-    elements:{
-      point:{
-        pointBorderColor:"red"
-      }
-    },
-    scales:{
-      y:{
-        ticks:{
-          color:"beige"
-        },
-        suggestedMin:50,
-        suggestedMax:100
-      },
-      x:{
-        ticks:{
-          color:"beige"
-        }
-      }
-    }
-  }
- });
+// new Chart(lineTrois,{
+//   type:'line',
+//   data: {
+//     labels:["3 nov 22", "10 nov 22", "19 nov 22","30 nov 22"],
+//     datasets:[{
+//     label:'# de tués',
+//     data:[5, 55, 15, 67],
+//     fill:true,
+//     backgroundColor:"yellow",
+//     tension:0.1
+//   }]
+// },
+//   options:{
+//     elements:{
+//       point:{
+//         pointBorderColor:"red"
+//       }
+//     },
+//     scales:{
+//       y:{
+//         ticks:{
+//           color:"beige"
+//         },
+//         suggestedMin:50,
+//         suggestedMax:100
+//       },
+//       x:{
+//         ticks:{
+//           color:"beige"
+//         }
+//       }
+//     }
+//   }
+//  });
 
- //POUR LES KIDNAPPES
+//  //POUR LES KIDNAPPES
 
- new Chart(lineTroisBis,{
-  type:'line',
-  data: {
-    labels:["1 nov 22", "8 nov 22", "15 nov 22","26 nov 22", "29 nov 22"],
-    datasets:[{
-    label:'# de kidnapés',
-    data:[70, 5, 40, 17, 65],
-    fill:true,
-    backgroundColor:"grey",
-    tension:0.1
-  }]
-},
-  options:{
-    elements:{
-      point:{
-        pointBorderColor:"#333"
-      }
-    },
-    scales:{
-      y:{
-        ticks:{
-          color:"beige"
-        },
-        suggestedMin:50,
-        suggestedMax:100
-      },
-      x:{
-        ticks:{
-          color:"beige"
-        }
-      }
-    }
-  }
- });
-
-
+//  new Chart(lineTroisBis,{
+//   type:'line',
+//   data: {
+//     labels:["1 nov 22", "8 nov 22", "15 nov 22","26 nov 22", "29 nov 22"],
+//     datasets:[{
+//     label:'# de kidnapés',
+//     data:[70, 5, 40, 17, 65],
+//     fill:true,
+//     backgroundColor:"grey",
+//     tension:0.1
+//   }]
+// },
+//   options:{
+//     elements:{
+//       point:{
+//         pointBorderColor:"#333"
+//       }
+//     },
+//     scales:{
+//       y:{
+//         ticks:{
+//           color:"beige"
+//         },
+//         suggestedMin:50,
+//         suggestedMax:100
+//       },
+//       x:{
+//         ticks:{
+//           color:"beige"
+//         }
+//       }
+//     }
+//   }
+//  });
 
 
-
+//REPONDRE A DES QUESTIONS DE L'UTILISATEUR
+  
 
 
